0c96c7901cdf79f440ca8cf83c0f23073ed02091
James Ashford <james.ashford@bnc.ox.ac.uk>
2021-05-07 00:30:26 +0100

    You can make amazing patterns by slightly changing the inital state of different colour channels

--- curiosities/CA_multi_self_reproduce.py
+++ curiosities/CA_multi_self_reproduce.py
@@ -5,23 +5,25 @@ import pyqtgraph as pg
 import numpy as np
 
 # Setup start configuration
-MAP_SIZE = 101
+MAP_SIZE = 201
 grid = np.zeros(shape=(MAP_SIZE, MAP_SIZE, 3))
-grid[49:52, 49:52, 0] = [
-    [1, 1, 1],
-    [1, 0, 1],
-    [1, 1, 1]
-]
-grid[49:52, 49:52, 1] = [
-    [0, 1, 0],
-    [1, 0, 1],
-    [0, 1, 0]
-]
-grid[49:52, 49:52, 2] = [
+mid = int(MAP_SIZE/2)
+l, h = mid-1, mid+2
+grid[l:h, l:h, 1] = [
     [1, 0, 1],
     [0, 0, 0],
     [1, 0, 1]
 ]
+grid[l:h, l:h, 2] = [
+    [0, 1, 0],
+    [1, 0, 1],
+    [0, 1, 0]
+]
+# grid[l:h, l:h, 2] = [
+#     [1, 0, 1],
+#     [0, 0, 0],
+#     [1, 0, 1]
+# ]
 
 # Setup window to show images
 app = pg.mkQApp()
@@ -29,6 +31,8 @@ win = QtGui.QMainWindow()
 win.setWindowTitle('Self-Reproducing CA')
 win.resize(800,800)
 imv = pg.ImageView()
+imv.ui.roiBtn.hide()
+imv.ui.menuBtn.hide()
 win.setCentralWidget(imv)
 win.show()
 
@@ -36,20 +40,24 @@ def iterate(start_grid):
     for dim in range(3):
         start_grid[:, :, dim] = convolve2d(
             start_grid[:, :, dim], 
-            [[1, 1, 1], [1, 0, 1], [1, 1, 1]], 
+            [
+                [1, 1, 1], 
+                [1, 0, 1], 
+                [1, 1, 1]
+            ], 
             boundary="fill", 
             mode="same"
-        ) % 4
+        ) % 200
     return start_grid
 
-# # Run Indefinitely
+# Run Indefinitely
 # def update():
 #     global grid
 #     grid = iterate(grid)
 #     imv.setImage(grid)
 # timer = QtCore.QTimer()
 # timer.timeout.connect(update)
-# timer.start(50)
+# timer.start(100)
 
 # Run for x frames and then view
 frames = []
@@ -57,7 +65,6 @@ for _ in range(100):
     frames.append(grid.copy())
     grid = iterate(grid)
 data = np.array(frames)
-print(data.shape)
 imv.setImage(data, xvals=np.arange(data.shape[0]+1))
 
 if __name__ == '__main__':
